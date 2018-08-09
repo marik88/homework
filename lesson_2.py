ba33a2d3a897a51648264dd69dd3b9d6e2e9ee88
Maryan Kleban <marik.wild@gmail.com>
2018-08-09 23:08:35 +0300

    lesson2

---
+++ lesson_2.py
@@ -0,0 +1,11 @@
+var_int = 10
+var_float = 8.4
+var_str = "No"
+
+big_int = var_int*5
+var_float -= 1
+print(var_int/var_float)
+print(big_int/var_float)
+var_str = var_str*2 + "Yes"*3
+
+print(f"big_int = {big_int}, var_float = {var_float}, var_str = {var_str}")
---
+++ test.py
@@ -0,0 +1,4 @@
+print((1+1)**4)
+print(125/36)
+print(15*30)
+print("Phython, Good Start")
